ef8366c525f082546698dcbfd62bc64a43645b3d
andrasnett <andras.nett@gmail.com>
2016-12-15 11:13:24 +0100

    03

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,7 +3,6 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img id="image">');
-//var $caption = $("<p></p>");
 var $title = $('<p id="title"></p>');
 var $leftArrow = $('<img id="previous" src="photos/left_arrow.png">');
 var $rightArrow = $('<img id="next" src="photos/right_arrow.png">');
@@ -11,6 +10,17 @@ var $rightArrow = $('<img id="next" src="photos/right_arrow.png">');
 // Variable to keep track of which overlay
 // image is in use
 var $index = 0;
+
+// Get number of images on page so we know when we are at last image
+var $galleryLength = $("#imageGallery a").length - 1;
+
+//Function to update overlay image and caption
+var updateImage = function(imageLocation, imageTitle){
+  //update image
+  $image.attr("src", imageLocation);
+  //update caption
+  $title.text(imageTitle);
+};
 
 //An image to overlay
 $overlay.append($image);
@@ -35,11 +45,13 @@ $("#imageGallery a").click(function (event) {
     var imageLocation = $(this).attr("href");
     var imageTitle = $(this).attr("title");
 
-    //Update overlay with the image linked in the link
-    $image.attr("src", imageLocation);
+    //update index to current selected image	
+	$index = $(this).index();
 
-    //Update overlay with the title
-    $title.text(imageTitle);
+    /* Update overlay image and caption
+	   with the image that was clicked on
+	   by using updateImage function */
+	updateImage(imageLocation, imageTitle);
 
     //Show the overlay
     $overlay.show();
@@ -53,13 +65,46 @@ $overlay.click(function(){
   $overlay.hide();
 });
 
-//Capture the click event on the arrows
-$("#previous").click(function () {
-    var href = $(this).attr("href");
-    console.log(href)
+//Arrows function
+var arrows = function (left) {
+    //set arrows to true to move backwards in the index
+
+    // if right arrow is clicked, add 1 to index
+    // else (if left arrow is clicked) subtract 1
+    if (!left) { $index++; }
+    else { $index--; }
+
+    //if out of index reset
+    if ($index < 0) { $index = $galleryLength; }
+    if ($index > $galleryLength) { $index = 0; }
+
+    // Get next <a>
+    var newImageSelected = $("#imageGallery a").get($index);
+
+    //grab link information
+    var imageLocation = $(newImageSelected).attr("href");
+    var imageTitle = $(newImageSelected).attr("title");
+
+    //Update Overlay
+    updateImage(imageLocation, imageTitle);
+}
+
+//Button events
+
+$("#previous").click(function(event){
+  arrows(true);
+  console.log($index + ' left');
 });
 
-$("#next").click(function () {
-    var href = $(this).attr("href");
-    console.log(href)
+$("#next").click(function(event){
+  arrows();
+  console.log($index + ' right');
 });
+
+
+
+
+
+
+
+
